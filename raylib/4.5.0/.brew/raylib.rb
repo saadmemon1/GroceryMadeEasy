8faf0e51411bb6eb3c7a2807fe134242416c0895
Saad Inam <saadinam.mn@gmail.com>
2024-04-15 14:46:50 +0500

    Added raylib folder and made it compatible with every system

---
+++ raylib/4.5.0/.brew/raylib.rb
@@ -0,0 +1,67 @@
+class Raylib < Formula
+  desc "Simple and easy-to-use library to learn videogames programming"
+  homepage "https://www.raylib.com/"
+  url "https://github.com/raysan5/raylib/archive/4.5.0.tar.gz"
+  sha256 "0df98bfc553db31356cab46a2f9ed6d932065f186a0fff24bafa05f8a60e16d1"
+  license "Zlib"
+  head "https://github.com/raysan5/raylib.git", branch: "master"
+
+  depends_on "cmake" => :build
+
+  on_linux do
+    depends_on "libx11"
+    depends_on "libxcursor"
+    depends_on "libxi"
+    depends_on "libxinerama"
+    depends_on "libxrandr"
+    depends_on "mesa"
+    depends_on "mesa-glu"
+  end
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+                               "-DBUILD_SHARED_LIBS=ON",
+                               "-DMACOS_FATLIB=OFF",
+                               "-DBUILD_EXAMPLES=OFF",
+                               "-DBUILD_GAMES=OFF",
+                               *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    system "cmake", "-S", ".", "-B", "build-static",
+                               "-DBUILD_SHARED_LIBS=OFF",
+                               "-DMACOS_FATLIB=OFF",
+                               "-DBUILD_EXAMPLES=OFF",
+                               "-DBUILD_GAMES=OFF",
+                               *std_cmake_args
+    system "cmake", "--build", "build-static"
+    lib.install "build-static/raylib/libraylib.a"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdlib.h>
+      #include <raylib.h>
+      int main(void)
+      {
+          int num = GetRandomValue(42, 1337);
+          return 42 <= num && num <= 1337 ? EXIT_SUCCESS : EXIT_FAILURE;
+      }
+    EOS
+    flags = if OS.mac?
+      %w[
+        -framework Cocoa
+        -framework IOKit
+        -framework OpenGL
+      ]
+    else
+      %w[
+        -lm
+        -ldl
+        -lGL
+        -lpthread
+      ]
+    end
+    system ENV.cc, "test.c", "-o", "test", "-L#{lib}", "-lraylib", *flags
+    system "./test"
+  end
+end
